98ec127b9131fdf3e1016b4d3b46d5c49f2af3a1
tpkeeper <tpkeeper.me@gmail.com>
2021-03-17 22:14:48 +0800

    test: check multi staker deposit and withdraw

--- test/FISDrop.test.js
+++ test/FISDrop.test.js
@@ -108,6 +108,122 @@ describe("FISDrop", function () {
         })
 
 
+        it("fis distribute amount should match totalReward", async function () {
+            this.fisDrop = await this.FISDrop.deploy(this.fisToken.address)
+            await this.fisDrop.deployed()
+            const startBlock = "100"
+            const rewardPerBlock = "10"
+            const totalReward = "102"
+            const claimableStartBlock = "130"
+            const lockedEndBlock = "140"
+
+            await this.fisDrop.add(this.lp.address, startBlock, rewardPerBlock, totalReward, claimableStartBlock, lockedEndBlock)
+
+            await this.lp.connect(this.bob).approve(this.fisDrop.address, "1000")
+            await this.fisDrop.connect(this.bob).deposit(0, "100")
+            await time.advanceBlockTo("189")
+            expect(await this.fisDrop.getUserCurrentTotalReward("0", this.bob.address)).to.equal("102")
+        })
+
+        it("should not distribute fis if no one deposit", async function () {
+            this.fisDrop = await this.FISDrop.deploy(this.fisToken.address)
+            await this.fisDrop.deployed()
+            const startBlock = "100"
+            const rewardPerBlock = "10"
+            const totalReward = "102"
+            const claimableStartBlock = "130"
+            const lockedEndBlock = "140"
+
+            await this.fisDrop.add(this.lp.address, startBlock, rewardPerBlock, totalReward, claimableStartBlock, lockedEndBlock)
+
+            await this.lp.connect(this.bob).approve(this.fisDrop.address, "1000")
+            await time.advanceBlockTo("189")
+
+            const pool = await this.fisDrop.poolInfo("0")
+            expect(pool.leftReward).to.equal("102")
+        })
+
+        it("should distribute fis properly for each staker", async function () {
+            this.fisDrop = await this.FISDrop.deploy(this.fisToken.address)
+            await this.fisDrop.deployed()
+            const startBlock = "300"
+            const rewardPerBlock = "1000"
+            const totalReward = "700000"
+            const claimableStartBlock = "400"
+            const lockedEndBlock = "10000"
+
+            await this.fisDrop.add(this.lp.address, startBlock, rewardPerBlock, totalReward, claimableStartBlock, lockedEndBlock)
+
+            await this.lp.connect(this.alice).approve(this.fisDrop.address, "1000", {
+                from: this.alice.address,
+            })
+            await this.lp.connect(this.bob).approve(this.fisDrop.address, "1000", {
+                from: this.bob.address,
+            })
+            await this.lp.connect(this.carol).approve(this.fisDrop.address, "1000", {
+                from: this.carol.address,
+            })
+            // Alice deposits 10 LPs at block 310
+            await time.advanceBlockTo("309")
+            await this.fisDrop.connect(this.alice).deposit(0, "10", { from: this.alice.address })
+            // Bob deposits 20 LPs at block 314
+            await time.advanceBlockTo("313")
+            await this.fisDrop.connect(this.bob).deposit(0, "20", { from: this.bob.address })
+            // Carol deposits 30 LPs at block 318
+            await time.advanceBlockTo("317")
+            await this.fisDrop.connect(this.carol).deposit(0, "30", { from: this.carol.address })
+            // Alice deposits 10 more LPs at block 320. At this point:
+            // Alice should have: 4*1000 + 4*1/3*1000 + 2*1/6*1000 = 5666
+            // Bob should have: 4*2/3*1000 + 2*2/6*1000 = 3333
+            // Carol should have: 2*3/6*1000 = 1000
+            // Drop should have the remaining: 70000 - 10*1000 = 690000
+            await time.advanceBlockTo("319")
+            await this.fisDrop.connect(this.alice).deposit(0, "10", { from: this.alice.address })
+            var pool = await this.fisDrop.poolInfo("0")
+            expect(pool.leftReward).to.equal("690000")
+
+            expect(await this.fisDrop.getUserCurrentTotalReward("0", this.alice.address)).to.equal("5666")
+            expect(await this.fisDrop.getUserCurrentTotalReward("0", this.bob.address)).to.equal("3333")
+            expect(await this.fisDrop.getUserCurrentTotalReward("0", this.carol.address)).to.equal("1000")
+            // Bob withdraws 5 LPs at block 330. At this point:
+            // Alice should have: 4*1000 + 4*1/3*1000 + 2*1/6*1000 +10*2/7*1000= 8523
+            // Bob should have: 4*2/3*1000 + 2*2/6*1000 + 10*2/7*1000 = 6190
+            // Carol should have: 2*3/6*1000 + 10*3/7*1000 = 5285
+            await time.advanceBlockTo("329")
+            await this.fisDrop.connect(this.bob).withdraw(0, "5", { from: this.bob.address })
+            pool = await this.fisDrop.poolInfo("0")
+            expect(pool.leftReward).to.equal("680000")
+
+            expect(await this.fisDrop.getUserCurrentTotalReward("0", this.alice.address)).to.equal("8523")
+            expect(await this.fisDrop.getUserCurrentTotalReward("0", this.bob.address)).to.equal("6190")
+            expect(await this.fisDrop.getUserCurrentTotalReward("0", this.carol.address)).to.equal("5286")
+            // Alice withdraws 20 LPs at block 340.
+            // Bob withdraws 15 LPs at block 350.
+            // Carol withdraws 30 LPs at block 360.
+            await time.advanceBlockTo("339")
+            await this.fisDrop.connect(this.alice).withdraw(0, "20", { from: this.alice.address })
+            await time.advanceBlockTo("349")
+            await this.fisDrop.connect(this.bob).withdraw(0, "15", { from: this.bob.address })
+            await time.advanceBlockTo("359")
+            await this.fisDrop.connect(this.carol).withdraw(0, "30", { from: this.carol.address })
+
+            pool = await this.fisDrop.poolInfo("0")
+            expect(pool.leftReward).to.equal("650000")
+            // Alice should have: 5666 + 10*2/7*1000 + 10*2/6.5*1000 = 11600
+            expect(await this.fisDrop.getUserCurrentTotalReward("0", this.alice.address)).to.equal("11600")
+            // Bob should have: 6190 + 10*1.5/6.5 * 1000 + 10*1.5/4.5*1000 = 11831
+            expect(await this.fisDrop.getUserCurrentTotalReward("0", this.bob.address)).to.equal("11831")
+            // Carol should have: 2*3/6*1000 + 10*3/7*1000 + 10*3/6.5*1000 + 10*3/4.5*1000 + 10*1000 = 26568
+            expect(await this.fisDrop.getUserCurrentTotalReward("0", this.carol.address)).to.equal("26568")
+            // All of them should have 1000 LPs back.
+            expect(await this.lp.balanceOf(this.alice.address)).to.equal("1000")
+            expect(await this.lp.balanceOf(this.bob.address)).to.equal("1000")
+            expect(await this.lp.balanceOf(this.carol.address)).to.equal("1000")
+        })
+
+
+
+
     })
 
 })
